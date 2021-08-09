1eb761c731fab55faf4f1240f080340f7e55b019
Jigar Anam <61108395+anamjigar2000@users.noreply.github.com>
2021-08-09 22:43:07 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<!-- navbar --> 
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+
+        body{margin-top: 15px;
+        }
+       
+          .btn {
+                  border: 2px solid black;
+                  background-color: white;
+                  color: black;
+                  padding: 14px 28px;
+                  font-size: 16px;
+                  cursor: pointer;
+                  margin: 5px;
+                }
+            
+            .df:hover {
+        color: white;
+        font-size: 16px;
+    background-color: #aa4b6b;
+    background-image: linear-gradient(315deg, #aa4b6b 20%, #6b6b83 60%, #3b8d99 20%);
+
+}
+
+        </style>
+      </head>
+      <body>
+      
+      <nav  class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"><img src="img/logo.png"></a>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">Home</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="contact.php">Contact Me</a>
+              </li>
+          </div>
+       </nav>
+     </body>
+     </html>
